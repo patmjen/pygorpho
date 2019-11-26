58dc047d70267b31b16a53f43fecedabcc39926c
Patrick M. Jensen <patmjen@gmail.com>
2019-11-26 20:20:08 +0100

    Add pyproject.toml to MANIFEST
    This meant we had to bump the version so packages on PyPI could be
    upgraded.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as readme:
 
 setup(
     name='pygorpho',
-    version='0.2.3',
+    version='0.2.4',
     description='Python bindings for gorpho',
     url='https://pygorpho.readthedocs.io/',
     project_urls={
