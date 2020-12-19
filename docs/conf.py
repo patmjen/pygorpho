aeaab695df19737045f28e62785ae5f00b04d40b
Patrick M. Jensen <patmjen@gmail.com>
2020-12-19 14:43:58 +0100

    Bump version to 0.7.1

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = 'Patrick M. Jensen'
 # The short X.Y version
 version = '0.7'
 # The full version, including alpha/beta/rc tags
-release = '0.7.0'
+release = '0.7.1'
 
 
 # -- General configuration ---------------------------------------------------
